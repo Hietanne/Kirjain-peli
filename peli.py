ceb3a1e95d053bec8643a1e426e206ce400c1571
Onni Hietanen <hietanne@gmail.com>
2023-12-09 16:48:20 +0200

    Muutoksia

--- peli.py
+++ peli.py
@@ -4,19 +4,27 @@ import os
 
 # Käynnistetään palvelin kun painetaan näppäintä.
 def start_server():
+    btn_create_server.config(state="disabled")  # Estä nappi klikkaamisen jälkeen
     subprocess.Popen(["python", "palvelin.py"])
+    # Oletetaan, että palvelin kirjoittaa IP-osoitteen tiedostoon
+    try:
+        with open("server_ip.txt", "r") as file:
+            server_ip = file.read().strip()
+            lbl_server_ip.config(text=f"Server IP: {server_ip}")
+    except Exception as e:
+        lbl_server_ip.config(text="Error reading server IP")
 
 def join_game():
-    # This function will be called when the "Join Game" button is clicked
-    # Here, you can add the logic to join a game
     subprocess.Popen(["python", "JoinGame.py"])
 
 # Create the main window
 root = tk.Tk()
 root.title("LAN Lettergrid Game")
-
-# Set window resolution to 800x800
 root.geometry("400x400")
+
+# Luodaan Label IP-osoitteelle
+lbl_server_ip = tk.Label(root, text="Server IP: Not Started")
+lbl_server_ip.pack(pady=10)
 
 # Create and place buttons
 btn_create_server = tk.Button(root, text="Create Server", command=start_server)
--- startgame.py
+++ startgame.py
@@ -7,24 +7,36 @@ def fetch_game_board(server_ip, server_port):
         client_socket.connect((server_ip, server_port))
         game_board = client_socket.recv(4096).decode('utf-8')
         client_socket.close()
-        return game_board
+        return game_board.split('\n')  # Muutetaan pelilauta listaksi rivejä
     except Exception as e:
         print(f"Error connecting to game server: {e}")
-        return None
+        return []
+
+def create_board_frame(root, game_board):
+    board_frame = tk.Frame(root)
+    board_frame.pack()
+
+    for row_idx, row in enumerate(game_board):
+        for col_idx, letter in enumerate(row):
+            # Muutetaan kirjain isoksi kirjaimeksi
+            letter = letter.upper()
+            label = tk.Label(board_frame, text=letter, font=("Helvetica", 18), width=2, borderwidth=1, relief="solid")
+            label.grid(row=row_idx, column=col_idx)
+    return board_frame
 
 def main():
     root = tk.Tk()
     root.title("Game Screen")
-    root.geometry("600x600")  # Asetetaan ikkunan koko
+    root.geometry("600x600")
 
     # Lue IP-osoite tiedostosta
     with open("server_ip.txt", "r") as file:
         server_ip = file.read().strip()
     server_port = 12346
-    
+
     game_board = fetch_game_board(server_ip, server_port)
     if game_board:
-        tk.Label(root, text=game_board, font=("Helvetica", 12)).pack()
+        create_board_frame(root, game_board)
 
     root.mainloop()
 
